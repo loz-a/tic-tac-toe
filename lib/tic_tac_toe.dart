ab2559f7b2f58401f2aa2235b554a9a462cbc532
Andiry Loz <loz.andriy@gmail.com>
2023-06-02 17:19:25 +0300

    remove test comment

--- lib/tic_tac_toe.dart
+++ lib/tic_tac_toe.dart
@@ -8,7 +8,6 @@ import 'src/view/renderer/observer/render.dart';
 void main() {
 
   init();
-  'hello';
   var container = Container.getInstance();
 
   var state = container.get((State).toString()) as State;
